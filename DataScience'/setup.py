af3da718a4052be9feae7982f7885cc9b0c2193d
tjvick1115 <tjvick1115@gmail.com>
2022-06-22 21:22:56 -0500

    Cookiecutter Template

---
+++ DataScience'/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='repo to practice  science and ML',
+    author='tjvick1115',
+    license='MIT',
+)
